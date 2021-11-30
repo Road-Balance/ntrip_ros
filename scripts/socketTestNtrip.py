fdddd3a7aeeda13894c403e74934d9f884bda2af
kimsooyoung <tge1375@naver.com>
2021-12-01 03:04:16 +0900

    Feature : fkp recv done

--- scripts/socketTestNtrip.py
+++ scripts/socketTestNtrip.py
@@ -16,14 +16,21 @@ def getMountPointBytes(mountpoint, useragent, user):
    mountPointString+="\r\n"
    if verbose:
       print(mountPointString)
-   return bytes(mountPointString,'ascii')
+   return mountPointString
+   # return bytes(mountPointString,'ascii')
 
 target_host = "gnssdata.or.kr" 
 target_port = 2101  # create a socket object 
-
 mountpoint = "SOUL-RTCM23"
 useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
 user = "tge1375@naver.com:gnss"
+
+target_host = "fkp.ngii.go.kr" 
+target_port = 2201  # create a socket object 
+mountpoint = "SOUL-RTCM23"
+useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+user = "tge1375@naver.com:gnss"
+
 # user = base64.b64encode(bytes(user,'utf-8')).decode("utf-8")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
@@ -34,10 +41,15 @@ if error_indicator == 0:
    connectTime=datetime.datetime.now()
 
    client.settimeout(10)
-   client.sendall(getMountPointBytes(mountpoint, useragent, user))
-   while True:
+   # client.sendall(getMountPointBytes(mountpoint, useragent, user))
+   client.send(getMountPointBytes(mountpoint, useragent, user).encode())
+
+   toggle = True
+   while toggle:
       casterResponse = client.recv(4096) #All the data
       header_lines = casterResponse.decode('utf-8').split("\r\n")
       print(header_lines)
+
+      toggle = not toggle
 
 client.close()
