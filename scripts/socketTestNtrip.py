8cb4a66d19bc72044023e6d4b451e3d9057ea8c5
kimsooyoung <tge1375@naver.com>
2021-12-01 02:57:30 +0900

    first commit

---
+++ scripts/socketTestNtrip.py
@@ -0,0 +1,43 @@
+import socket
+import base64
+import datetime
+
+verbose = True
+V2 = True
+
+def getMountPointBytes(mountpoint, useragent, user):
+   mountPointString = "GET %s HTTP/1.1\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n" % (mountpoint, useragent, user)
+#        mountPointString = "GET %s HTTP/1.1\r\nUser-Agent: %s\r\n" % (self.mountpoint, useragent)
+   # if self.host or self.V2:
+   #    hostString = "Host: %s:%i\r\n" % (self.caster,self.port)
+   #    mountPointString+=hostString
+   if V2:
+      mountPointString+="Ntrip-Version: Ntrip/2.0\r\n"
+   mountPointString+="\r\n"
+   if verbose:
+      print(mountPointString)
+   return bytes(mountPointString,'ascii')
+
+target_host = "gnssdata.or.kr" 
+target_port = 2101  # create a socket object 
+
+mountpoint = "SOUL-RTCM23"
+useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+user = "tge1375@naver.com:gnss"
+# user = base64.b64encode(bytes(user,'utf-8')).decode("utf-8")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+
+error_indicator = client.connect_ex((target_host,target_port))
+if error_indicator == 0:
+   sleepTime = 1
+   connectTime=datetime.datetime.now()
+
+   client.settimeout(10)
+   client.sendall(getMountPointBytes(mountpoint, useragent, user))
+   while True:
+      casterResponse = client.recv(4096) #All the data
+      header_lines = casterResponse.decode('utf-8').split("\r\n")
+      print(header_lines)
+
+client.close()
