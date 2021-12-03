2bac6c9d87b04b6570f33ad19f0d4b133a15d1cf
kimsooyoung <tge1375@naver.com>
2021-12-04 02:08:30 +0900

    Feature : ntrip client done

--- scripts/socketTestNtrip.py
+++ scripts/socketTestNtrip.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import socket
 import base64
 import datetime
@@ -18,22 +19,24 @@ def getGGABytes():
       (now.hour,now.minute,now.second)
 
    checksum = calcultateCheckSum(ggaString)
-   # if self.verbose:
-   #    print  ("$%s*%s\r\n" % (ggaString, checksum))
+   if verbose:
+      print("$%s*%s\r\n" % (ggaString, checksum))
    return bytes("$%s*%s\r\n" % (ggaString, checksum),'ascii')
 
 def getMountPointBytes(mountpoint, useragent, user):
    mountPointString = "GET %s HTTP/1.1\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n" % (mountpoint, useragent, user)
-#        mountPointString = "GET %s HTTP/1.1\r\nUser-Agent: %s\r\n" % (self.mountpoint, useragent)
+   
    # if self.host or self.V2:
    #    hostString = "Host: %s:%i\r\n" % (self.caster,self.port)
-   #    mountPointString+=hostString
+   #    mountPointString += hostString
+   
    if V2:
       mountPointString+="Ntrip-Version: Ntrip/2.0\r\n"
    mountPointString+="\r\n"
+   
    if verbose:
       print(mountPointString)
-   return mountPointString
+   return mountPointString.encode()
    # return bytes(mountPointString,'ascii')
 
 target_host = "www.gnssdata.or.kr" 
@@ -50,93 +53,164 @@ user = "tge1375@naver.com:gnss"
 
 # user = base64.b64encode(bytes(user,'utf-8')).decode("utf-8")
 
+# 전역
+factor=2 # How much the sleep time increases with each failed attempt
+maxReconnect=1
+maxReconnectTime=1200
+sleepTime=1 # So the first one is 1 second
+
+client = None
+
+# 클래스 변수
+ssl = False
+maxConnectTime = 0
 found_header = False
 
+reconnectTry=1
+sleepTime=1
+if maxConnectTime > 0 :
+   EndConnect=datetime.timedelta(seconds=maxConnectTime)
 try:
-   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+   while reconnectTry<=maxReconnect:
+      found_header=False
+      if verbose:
+         print('Connection {0} of {1}\n'.format(reconnectTry,maxReconnect))
 
-   error_indicator = client.connect_ex((target_host,target_port))
-   if error_indicator == 0:
-      sleepTime = 1
-      connectTime=datetime.datetime.now()
+      client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+      if ssl:
+         client = ssl.wrap_socket(client)
+      
+      error_indicator = client.connect_ex((target_host,target_port))
+      if error_indicator == 0:
+         sleepTime = 1
+         connectTime=datetime.datetime.now()
 
-      client.settimeout(10)
-      client.send(getMountPointBytes(mountpoint, useragent, user).encode())
-      while not found_header:
-         casterResponse = client.recv(4096) #All the data
-         header_lines = casterResponse.decode('utf-8').split("\r\n")
-         print(header_lines)
+         client.settimeout(10)
+         # client.send(getMountPointBytes(mountpoint, useragent, user).encode())
+         client.send(getMountPointBytes(mountpoint, useragent, user))
 
-         for line in header_lines:
-            if line=="":
-               if not found_header:
-                  found_header=True
-                  # if self.verbose:
-                  #       sys.stderr.write("End Of Header"+"\n")
-            # else:
-            #    if self.verbose:
-            #       sys.stderr.write("Header: " + line+"\n")
-            # if self.headerOutput:
-            #    self.headerFile.write(line+"\n")
+         while not found_header:
+            casterResponse = client.recv(4096) #All the data
+            header_lines = casterResponse.decode('utf-8').split("\r\n")
+            print(header_lines)
 
-         for line in header_lines:
-            if line.find("SOURCETABLE")>=0:
-               sys.stderr.write("Mount point does not exist")
-               sys.exit(1)
-            elif line.find("401 Unauthorized")>=0:
-               sys.stderr.write("Unauthorized request\n")
-               sys.exit(1)
-            elif line.find("404 Not Found")>=0:
-               sys.stderr.write("Mount Point does not exist\n")
-               sys.exit(2)
-            elif line.find("ICY 200 OK")>=0:
-               #Request was valid
-               client.sendall(getGGABytes())
-            elif line.find("HTTP/1.0 200 OK")>=0:
-               #Request was valid
-               client.sendall(getGGABytes())
-            elif line.find("HTTP/1.1 200 OK")>=0:
-               #Request was valid
-               client.sendall(getGGABytes())
+            for line in header_lines:
+               if line=="":
+                  if not found_header:
+                     found_header=True
+                     if verbose:
+                        print("End Of Header"+"\n")
+               else:
+                  if verbose:
+                     print("Header: " + line+"\n")
 
+            for line in header_lines:
+               if line.find("SOURCETABLE")>=0:
+                  sys.stderr.write("Mount point does not exist")
+                  sys.exit(1)
+               elif line.find("401 Unauthorized")>=0:
+                  sys.stderr.write("Unauthorized request\n")
+                  sys.exit(1)
+               elif line.find("404 Not Found")>=0:
+                  sys.stderr.write("Mount Point does not exist\n")
+                  sys.exit(2)
+               elif line.find("ICY 200 OK")>=0:
+                  #Request was valid
+                  client.sendall(getGGABytes())
+               elif line.find("HTTP/1.0 200 OK")>=0:
+                  #Request was valid
+                  client.sendall(getGGABytes())
+               elif line.find("HTTP/1.1 200 OK")>=0:
+                  #Request was valid
+                  client.sendall(getGGABytes())
 
-      data = "Initial data"
-      while data:
-         try:
-            data = client.recv(50)
+         data = "Initial data"
+         RTCM_ARR = []
+         while data:
+            try:
+               data = client.recv(1)
+               if ord(data) == 211:
+                  RTCM_ARR.append(ord(data))
 
-            int_values = [x for x in data]
-            if int_values[0] == 211:
-               print(int_values)
-            # data = data.decode('ISO-8859-1').split("\r\n")
-            # data = data.decode('ascii')
-            # decoded_data = base64.b64decode(data)
-            # data = data.decode('ISO-8859-1')
-            # print(data[0])
-            # self.out.write(data)
-            # self.out.buffer.write(data)
-            # if self.UDP_socket:
-            #    self.UDP_socket.sendto(data, ('<broadcast>', self.UDP_Port))
-            # if self.maxConnectTime :
-            #    if datetime.datetime.now() > connectTime+EndConnect:
-            #       if self.verbose:
-            #             sys.stderr.write("Connection Time exceeded\n")
-            #       sys.exit(0)
-            # self.socket.sendall(self.getGGAString())
-         except socket.timeout:
-               # if self.verbose:
-               #    sys.stderr.write('Connection TimedOut\n')
+                  new_data = client.recv(2)
+                  print(new_data, type(new_data), new_data[0], new_data[1])
+                  
+                  RTCM_ARR.append(new_data[0])
+                  RTCM_ARR.append(new_data[1])
+                  print(RTCM_ARR)
+                  
+                  cnt = new_data[0] * 256 + new_data[1]
+                  cnt = cnt + 1
+                  print("cnt : ", cnt)
+
+                  if cnt > 255: # Just Abstract Value 
+                     print("Over Msg Error")
+                     RTCM_ARR = []
+                     continue
+
+                  for i in range(cnt):
+                     new_data = client.recv(1)
+                     RTCM_ARR.append(ord(new_data))
+
+                  print("Final Array : ", RTCM_ARR)
+                  ros_message = bytes(RTCM_ARR)
+                  RTCM_ARR = []
+               
+               if maxConnectTime :
+                  if datetime.datetime.now() > connectTime+EndConnect:
+                     if verbose:
+                        sys.stderr.write("Connection Time exceeded\n")
+                     sys.exit(0)
+            # TODO : TypeError: catching classes that do not inherit from BaseException is not allowed
+            except client.timeout:
+               if verbose:
+                  sys.stderr.write('Connection TimedOut\n')
                data = False
-         except socket.error:
-               # if self.verbose:
-               #    sys.stderr.write('Connection Error\n')
+
+            except client.error:
+               if verbose:
+                  sys.stderr.write('Connection Error\n')
                data = False
 
+            except IndexError:
+               RTCM_ARR = []
+               pass
+
+            except Exception as e:
+               print("Unknown Error : ", e)
+               pass
+               
+
+         if verbose:
+            sys.stderr.write('Closing Connection\n')
+         client.close()
+         client = None
+
+         if reconnectTry < maxReconnect :
+            sys.stderr.write( "%s No Connection to NtripCaster.  Trying again in %i seconds\n" % (datetime.datetime.now(), sleepTime))
+            time.sleep(sleepTime)
+            sleepTime *= factor
+
+            if sleepTime > maxReconnectTime:
+                  sleepTime = maxReconnectTime
+         else:
+            sys.exit(1)
+
+         reconnectTry += 1
+      else:
+         client = None
+         if verbose:
+            print("Error indicator: ", error_indicator)
+
+         if reconnectTry < maxReconnect :
+            sys.stderr.write( "%s No Connection to NtripCaster.  Trying again in %i seconds\n" % (datetime.datetime.now(), sleepTime))
+            time.sleep(sleepTime)
+            sleepTime *= factor
+            if sleepTime > maxReconnectTime:
+                  sleepTime = maxReconnectTime
+         reconnectTry += 1
 
 except KeyboardInterrupt:
    if client:
       client.close()
    sys.exit()
-
-
-client.close()
